3238486c7f2a81ee48b1a310d8f86b8f27eabd41
Samuel Majerus <ladolego@gmail.com>
2022-08-29 11:50:26 -0700

    Initial commit of project with 'webpack' and 'webpack-cli' dependencies... [start of Wk5, Lsn 10]

---
+++ src/triangle.js
@@ -0,0 +1,10 @@
+/*     Business Logic     */ 
+function Triangle(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
+}
+
+Triangle.prototype.checkType = function() {
+  return "I can't answer that yet!";
+}
