83770517e7afead16e824ccf4e83a71aeda3f031
Samuel Majerus <ladolego@gmail.com>
2022-08-29 18:51:19 -0700

    End of code-along work for Wk5, Lsn. 14-16.

---
+++ triangle.js
@@ -0,0 +1,3 @@
+export default function Triangle(side1, side2, side3) {
+  ...
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',  // new line
+  plugins: [
+    new CleanWebpackPlugin({
+      verbose: true
+    }), // new line
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
