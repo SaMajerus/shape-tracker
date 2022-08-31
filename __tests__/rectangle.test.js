602725bde8aa2dea67222ee64ae837cdacb2489d
Samuel Majerus <ladolego@gmail.com>
2022-08-30 22:24:39 -0700

    DRY'd the rectangle tests

--- __tests__/rectangle.test.js
+++ __tests__/rectangle.test.js
@@ -1,14 +1,18 @@
 import Rectangle from '../src/js/rectangle.js';
 
 describe('Rectangle', () => {
+  let rectangle;
+
+  beforeEach(() => {
+    rectangle = new Rectangle(3,5);
+  });
 
   test('should correctly create a rectangle object using two sides', () => {
-    const rectangle = new Rectangle(3,5);
     expect(rectangle.side1).toEqual(3);
     expect(rectangle.side2).toEqual(5);
   });
+
   test('should correctly get the area of a rectangle object', () => {
-    const rectangle = new Rectangle(3,5);
     expect(rectangle.getArea()).toEqual(15);
   });
 });
